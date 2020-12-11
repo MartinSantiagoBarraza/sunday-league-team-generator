32f3a2864b720944ccea18ed84959abceb531d78
MartinSantiagoBarraza <martinsbarraza@gmail.com>
2020-12-11 18:16:57 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+#1. Program's greeting:
+print("Welcome to the Sunday League Team Generator!")
+#2. Ask user for the school they studied in:
+school_name = input("Type in the name of your school: \n")
+#3. Ask user for the name of their pet:
+pet_name = input("Type in the name of your pet: \n")
+#4. Combine name of their school and pet and show their team's name:
+print("The name of your Sunday League Team could be: " + school_name + " " + pet_name)
